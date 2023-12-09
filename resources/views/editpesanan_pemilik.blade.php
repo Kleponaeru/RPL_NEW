879c03f349d868111cbd7e39cb774c38e60d67f3
Leo Saputra <125854171+leosaputra0@users.noreply.github.com>
2023-12-09 19:25:08 +0700

    editpesananpemilik

--- resources/views/editpesanan_pemilik.blade.php
+++ resources/views/editpesanan_pemilik.blade.php
@@ -28,6 +28,20 @@
                                         </select>
                                     </div>
                                 </div>
+                                <div class="mb-3 row align-items-center">
+                                    <label for="id_location" class="col-sm-4 col-form-label">Bank Sampah</label>
+                                    <div class="col-sm-8">
+                                        <select class="form-select" aria-label="Bank Sampah" name="id_location">
+                                            <option selected disabled>Pilih Bank Sampah</option>
+                                            <option value=1 {{ $n->id_location == 1 ? 'selected' : '' }}>TPA Kota Baru
+                                            </option>
+                                            <option value=2 {{ $n->id_location == 2 ? 'selected' : '' }}>
+                                                TPA Sampah Karanganyar</option>
+                                            <option value=3 {{ $n->id_location == 3 ? 'selected' : '' }}>TPA Amplaz
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
                                 <div class="mb-3 row">
                                     <label for="jenis" class="col-sm-4 col-form-label">Pilih Jam</label>
                                     <div class="col-sm-8">
@@ -64,6 +78,14 @@
                                     </div>
                                 </div>
 
+                                <div class="mb-3 row align-items-center">
+                                    <label for="harga" class="col-sm-4 col-form-label">Harga</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" name='harga' class="form-control" id="harga" readonly>
+                                        <div id="formattedHarga"></div>
+                                    </div>
+                                </div>
+
                                 <div class="col-md-12 px-4 text-center mb-4">
                                     <button type="submit" class="btn btn-primary px-4" name="status"
                                         value="Process">Submit</button>
@@ -75,4 +97,33 @@
             </div>
         </div>
     </main>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var beratInput = document.getElementById('berat');
+            var hargaInput = document.getElementById('harga');
+            var formattedHargaDiv = document.getElementById('formattedHarga');
+
+            beratInput.addEventListener('input', function() {
+                var berat = parseFloat(beratInput.value) || 0;
+                var harga = calculateHarga(berat);
+                hargaInput.value = harga; // Remove .toFixed(2) if harga is an integer
+
+                // Format and display harga with Rupiah symbol
+                formattedHargaDiv.innerText = formatRupiah(harga);
+            });
+
+            function calculateHarga(berat) {
+                // You can define your pricing logic here
+                // For example, let's say the price is $1.50 per kilogram
+                var hargaPerKg = 5000;
+                return Math.round(berat * hargaPerKg); // Round to the nearest integer
+            }
+
+            function formatRupiah(number) {
+                // Format the number as Rupiah
+                var rupiah = 'Rp ' + number.toLocaleString('id-ID');
+                return rupiah;
+            }
+        });
+    </script>
 @endsection
